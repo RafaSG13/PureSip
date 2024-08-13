22fbfd46af67e9a79a17504a4ff253485068d9dc
RafaSG13 <=>
2024-08-13 15:20:24 +0200

    Initial Commit

---
+++ PureSip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PureSip
+//
+//  Created by Rafael Serrano Gamarra on 13/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PureSip/PureSipApp.swift
@@ -0,0 +1,17 @@
+//
+//  PureSipApp.swift
+//  PureSip
+//
+//  Created by Rafael Serrano Gamarra on 13/8/24.
+//
+
+import SwiftUI
+
+@main
+struct PureSipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
